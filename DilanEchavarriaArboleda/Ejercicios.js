b855a45330281f3995bd98bdfd7e39d7e2839f3f
Dilan <dilanechavarria68@gmail.com>
2025-11-13 11:42:07 -0500

    feat se realizo el segundo ejercicio

--- DilanEchavarriaArboleda/Ejercicios.js
+++ DilanEchavarriaArboleda/Ejercicios.js
@@ -1,13 +1,25 @@
 /*Necesito un programa que calcule el total de mi compra en el supermercado.
  Pide el precio de tres productos diferentes y muestra el total a pagar.*/
 
- // Pedir el precio de tres productos
 let producto1 = Number(prompt("Ingresa el precio del primer producto:"));
 let producto2 = Number(prompt("Ingresa el precio del segundo producto:"));
 let producto3 = Number(prompt("Ingresa el precio del tercer producto:"));
 
-// Calcular el total
 let total = producto1 + producto2 + producto3;
 
-// Mostrar el resultado
 alert("El total a pagar es: $" + total);
+
+/*Para un evento, necesito un banner. Pide el **ancho y el alto** de una pared
+ (en metros) y calcula su **área** para saber cuánto material comprar.*/
+let ancho = parseFloat(prompt("Ingresa el ancho de la pared (en metros):"));
+let alto = parseFloat(prompt("Ingresa el alto de la pared (en metros):"));
+
+let area = ancho * alto;
+
+alert("El área de la pared es: " + area + " metros cuadrados.");
+
+
+
+
+
+
